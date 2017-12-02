5001319ea727e82688ed4fb9be374ae2d745bfbb
tonianelope <tonianelope@yahoo.com>
2017-12-02 20:58:22 +0000

    + login button

--- playground/pyGithub/login.py
+++ playground/pyGithub/login.py
@@ -1,11 +1,11 @@
-import github 
+import github
 import os
-from flask import Flask, redirect, url_for, session, request
+from flask import Flask, redirect, url_for, session, request, render_template
 from flask_oauth import OAuth
 from flask.json import jsonify
 
 
-#SECRET_KEY = os.environ.get("AGITHUB_FSKEY", "")
+SECRET_KEY = os.environ.get("AGITHUB_FSKEY", "")
 
 GITHUB_APP_KEY = '754c9740c776a9321668'
 GITHUB_APP_SECRET = os.environ.get("AGITHUB_SKEY", "")
@@ -29,20 +29,24 @@ github = oauth.remote_app(
 
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    print('1')
+    return render_template("index.html")
+    #return redirect(url_for('login'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    print('2')
     return github.authorize(
         callback=url_for(
             'github_auth',
-            next=request.args.get('next') or request.refferrer or None,
+            next=request.args.get('next') or request.referrer or None,
             _external=True
         )
     )
 
 @app.route('/auth')
 def github_auth(resp):
+    print('3')
     if resp is None:
         return 'Access denied: reason={} error={}'.format(
             request.args['error_reason'],
@@ -53,6 +57,7 @@ def github_auth(resp):
 
 @app.route("/profile", methods=["GET"])
 def profile():
+    print('4')
     gh = github.Github(token)
     repos = ''
     for repo in gh.get_user().get_repos():
@@ -64,6 +69,6 @@ def profile():
 if __name__ == "__main__":
     # This allows us to use a plain HTTP callback
     os.environ['DEBUG'] = "1"
-
+    print('0')
     app.secret_key = os.urandom(24)
     app.run(debug=True)
